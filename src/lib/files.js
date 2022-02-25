65cdf0cc72f1f64899d73e62f0073dc88f8bb958
Bertrand DELION <bertrand.delion@free.fr>
2022-02-25 14:20:11 +0100

    Add options and control with yargs Rename src/index.js in src/clis.js

--- src/lib/files.js
+++ src/lib/files.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-// import fs from 'fs';
 const path = require('path');
-// import path from 'path';
 
 function getCurrentDirectoryBase() {
   return path.basename(process.cwd());
