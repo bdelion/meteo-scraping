7e99a2ffab9a79b72d7598b099023848f7272b0c
Bertrand DELION <bertrand.delion@free.fr>
2023-04-24 22:26:59 +0200

    TS

--- src/lib/files.js
+++ src/lib/files.js
@@ -3,12 +3,29 @@
 const fs = require('fs');
 const path = require('path');
 
+/**
+ * Renvoie le nom du répertoire courant
+ * @returns {string} Le nom du répertoire courant
+ */
 function getCurrentDirectoryBase() {
   return path.basename(process.cwd());
 }
 
-function directoryExists(filePath) {
-  return fs.existsSync(filePath);
+/**
+ * Vérifie si un répertoire existe à l'emplacement spécifié
+ * @param {string} dirPath - Le chemin du répertoire à vérifier
+ * @returns {boolean} True si le répertoire existe, false sinon
+ */
+function directoryExists(dirPath) {
+  try {
+    return fs.statSync(dirPath).isDirectory();
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      return false;
+    } else {
+      throw err;
+    }
+  }
 }
 
 module.exports = {
---
+++ src/test/files.test.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const { getCurrentDirectoryBase, directoryExists } = require('../lib/files');
+
+describe('getCurrentDirectoryBase', () => {
+  test('should return the current directory name', () => {
+    const currentDirName = path.basename(process.cwd());
+    expect(getCurrentDirectoryBase()).toEqual(currentDirName);
+  });
+});
+
+describe('directoryExists', () => {
+  test('should return true if directory exists', () => {
+    expect(directoryExists('./')).toEqual(true);
+  });
+
+  test('should return false if directory does not exist', () => {
+    expect(directoryExists('./non-existent-dir')).toEqual(false);
+  });
+
+  test('should throw error if access denied', () => {
+    expect(() => {
+      directoryExists('/root');
+    }).toThrow();
+  });
+});
