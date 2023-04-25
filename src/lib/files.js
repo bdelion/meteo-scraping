894a5f3937abf3e325b5f30524f7c365be9caae6
Bertrand DELION <bertrand.delion@free.fr>
2023-04-25 09:52:16 +0200

    TS

--- src/lib/files.js
+++ src/lib/files.js
@@ -14,14 +14,32 @@ function getCurrentDirectoryBase() {
 /**
  * Vérifie si un répertoire existe à l'emplacement spécifié
  * @param {string} dirPath - Le chemin du répertoire à vérifier
- * @returns {boolean} True si le répertoire existe, false sinon
+ * @param {boolean} recursive - Si vrai, vérifie récursivement les répertoires parents
+ * @returns {Promise<boolean>} Une promesse qui résout en true si le répertoire existe, false sinon
  */
-function directoryExists(dirPath) {
+async function directoryExists(dirPath, recursive = false) {
+  if (!dirPath || typeof dirPath !== 'string') {
+    throw new Error('Le chemin doit être une chaîne de caractères valide.');
+  }
+
   try {
-    return fs.statSync(dirPath).isDirectory();
+    const stats = await fs.promises.stat(path.normalize(dirPath));
+    if (stats.isDirectory()) {
+      return true;
+    } else if (!recursive) {
+      return false;
+    } else {
+      const parentDir = path.dirname(dirPath);
+      if (parentDir === dirPath) {
+        return false;
+      }
+      return directoryExists(parentDir, true);
+    }
   } catch (err) {
     if (err.code === 'ENOENT') {
       return false;
+    } else if (err.code === 'EPERM' || err.code === 'EACCES') {
+      throw new Error(err.code);
     } else {
       throw err;
     }
--- src/test/files.test.js
+++ src/test/files.test.js
@@ -1,19 +1,7 @@
-const fs = require('fs');
-const os = require('os');
 const path = require('path');
+const mockFs = require('mock-fs');
+
 const { getCurrentDirectoryBase, directoryExists } = require('../lib/files');
-
-const mockDirPath = path.join(os.tmpdir(), 'mocked-dir');
-
-beforeAll(() => {
-  // Create a temporary directory with root rights
-  fs.mkdirSync(mockDirPath, { recursive: true, mode: 0o777 });
-});
-
-afterAll(() => {
-  // Remove the temporary directory
-  fs.rmdirSync(mockDirPath);
-});
 
 describe('getCurrentDirectoryBase', () => {
   test('should return the current directory name', () => {
@@ -24,26 +12,58 @@ describe('getCurrentDirectoryBase', () => {
 
 describe('directoryExists', () => {
   beforeEach(() => {
-    // Mock the directory
-    jest.mock('/root', () => mockDirPath);
+    mockFs({
+      '/data/src/lib': mockFs.directory({
+        mode: '0755',
+        items: {
+          file1: 'file one content'
+        }
+      }),
+      '/inaccessible': mockFs.directory({
+        mode: '0000',
+      }),
+      'file': mockFs.file({
+        content: 'file content here',
+        ctime: new Date(1),
+        mtime: new Date(1)
+      })
+    });
   });
 
   afterEach(() => {
-    // Reset the mock
-    jest.resetModules();
+    mockFs.restore();
   });
 
-  test('should return true if directory exists', () => {
-    expect(directoryExists('./')).toEqual(true);
+  it('returns true for an existing directory', async () => {
+    const exists = await directoryExists('./');
+    expect(exists).toBe(true);
   });
 
-  test('should return false if directory does not exist', () => {
-    expect(directoryExists('./non-existent-dir')).toEqual(false);
+  it('returns false for a non-existing directory', async () => {
+    const exists = await directoryExists('./some/nonexistent/directory');
+    expect(exists).toBe(false);
   });
 
-  test('should throw error if access denied', () => {
-    expect(() => {
-      directoryExists('/root');
-    }).toThrow();
+  it('returns false for a non-existing directory recursively', async () => {
+    const exists = await directoryExists('/data/src/lib/files/file1', true);
+    expect(exists).toBe(false);
+  });
+
+  it('returns true for an existing directory recursively', async () => {
+    const exists = await directoryExists('/data/src/lib/file1', true);
+    expect(exists).toBe(true);
+  });
+
+  it('returns false for an existing directory not recursively', async () => {
+    const exists = await directoryExists('/data/src/lib/file1', false);
+    expect(exists).toBe(false);
+  });
+
+  it('throws an error for an invalid path', async () => {
+    await expect(directoryExists(null)).rejects.toThrow('Le chemin doit être une chaîne de caractères valide.');
+  });
+
+  it('throws an error if access denied', async () => {
+    await expect(directoryExists('/inaccessible/test.keystore')).rejects.toThrow('EACCES');
   });
 });
